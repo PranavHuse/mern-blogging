329a46d0d939d80d9bc4dc4486feaf13a5e5a18c
Pranav Huse <pranavhuse14@gmail.com>
2024-10-08 19:39:49 +0530

    error handler utils

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 
 dotenv.config();
 
@@ -13,6 +15,7 @@ mongoose.connect(process.env.MONGO)
   .catch((err) => {
     console.log(err);
   });
+app.use(express.json());
 
 
 
@@ -22,3 +25,13 @@ app.listen(3000, () => {
   });
 
 app.use('/api/user',userRoutes)  ;
+app.use('/api/auth',authRoutes) ;
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
