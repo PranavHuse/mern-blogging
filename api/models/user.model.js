f27d0daf659aa2d7ef60d4b84e545c8a25cd6328
Pranav Huse <pranavhuse14@gmail.com>
2024-10-15 22:20:22 +0530

    update  header for sign in

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     profilePicture: {
       type: String,
       default:
-        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1rNuFRQJ0m9EkNrwaJtyxCSEfY7Rz35rC_g&s",
     },
     isAdmin: {
       type: Boolean,
