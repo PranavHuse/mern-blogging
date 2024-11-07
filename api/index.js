f5363b1ea83d1a33a83e875f51adb26ec35dbad5
Pranav Huse <pranavhuse14@gmail.com>
2024-11-07 23:45:46 +0530

    post route and post image upload

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import { v2 as cloudinary } from "cloudinary";
 import cookieParser from 'cookie-parser';
-
+import postRoutes from './routes/post.route.js';
 dotenv.config();
 
 
@@ -29,6 +29,7 @@ app.listen(3000, () => {
 
 app.use('/api/user',userRoutes)  ;
 app.use('/api/auth',authRoutes) ;
+app.use('/api/post', postRoutes);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,70 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
 import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
 export default function CreatePost() {
+  const [file, setFile] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const handleUpdloadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError('Please select an image');
+        return;
+      }
+      const data = new FormData();
+      data.append("file", file);
+      data.append("upload_preset", "firstpreset");
+      data.append("cloud_name", import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
+      const res = await fetch(
+        "https://api.cloudinary.com/v1_1/" +
+          import.meta.env.VITE_CLOUDINARY_CLOUD_NAME +
+          "/upload",
+        {
+          method: "POST",
+          body: data,
+        }
+      );
+      const imgurl = await res.json();
+      // console.log(imgurl.secure_url);
+     //setImageFileUrl(imgurl.secure_url);
+      const downloadUrl=imgurl.secure_url;
+      //setFormData({ ...formData, profilePicture: downloadUrl });
+      setFormData({ ...formData, image: downloadUrl});
+      //setImageFileUploading(false);
+      setImageUploadError(null);
+      
+    } catch (error) {
+      setImageUploadError('Image upload failed');
+     // setImageUploadProgress(null);
+      console.log(error);
+    }
+  };
+  const handleSubmit = async (e) => {
+    // e.preventDefault();
+    // try {
+    //   const res = await fetch('/api/post/create', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(formData),
+    //   });
+    //   const data = await res.json();
+    //   if (!res.ok) {
+    //     setPublishError(data.message);
+    //     return;
+    //   }
+
+    //   if (res.ok) {
+    //     setPublishError(null);
+    //     navigate(`/post/${data.slug}`);
+    //   }
+    // } catch (error) {
+    //   setPublishError('Something went wrong');
+    // }
+  };
+
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
@@ -28,19 +91,26 @@ export default function CreatePost() {
           <FileInput
             type='file'
             accept='image/*'
-      
+            onChange={(e) => setFile(e.target.files[0])}
           />
           <Button
             type='button'
            
             size='sm'
             outline
-            
+            onClick={handleUpdloadImage}
           >
             Upload Image
           </Button>
         </div>
-       
+        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt='upload'
+            className='w-full h-72 object-cover'
+          />
+        )}
         <ReactQuill
           theme='snow'
           placeholder='Write something...'
