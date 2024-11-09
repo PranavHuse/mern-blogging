6913050582b0943c7aebfbe47437597c98369183
Pranav Huse <pranavhuse14@gmail.com>
2024-11-09 22:15:58 +0530

    show more posts

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -20,9 +20,9 @@ export default function DashPosts() {
             const data = await res.json();
             if (res.ok) {
               setUserPosts(data.posts);
-              console.log(data.posts);
+              //console.log(data.posts);
               if (data.posts.length < 9) {
-               // setShowMore(false);
+                setShowMore(false);
               }
             }
           } catch (error) {
@@ -35,6 +35,23 @@ export default function DashPosts() {
       }, [currentUser._id]);
     
  
+      const handleShowMore = async () => {
+        const startIndex = userPosts.length;
+        try {
+          const res = await fetch(
+            `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+          );
+          const data = await res.json();
+          if (res.ok) {
+            setUserPosts((prev) => [...prev, ...data.posts]);
+            if (data.posts.length < 9) {
+              setShowMore(false);
+            }
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
  
  
  
@@ -105,14 +122,14 @@ export default function DashPosts() {
                 </Table.Body>
               ))}
             </Table>
-            {/* {showMore && (
+            {showMore && (
               <button
-               // onClick={handleShowMore}
+               onClick={handleShowMore}
                 className='w-full text-teal-500 self-center text-sm py-7'
               >
                 Show more
               </button>
-            )} */}
+            )} 
           </>
         ) : (
           <p>You have no posts yet!</p>
